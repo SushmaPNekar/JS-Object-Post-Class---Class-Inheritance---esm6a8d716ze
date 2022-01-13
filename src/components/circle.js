a32172623997f522de047903034f0a4daf21ebfe
SushmaPNekar <87525377+SushmaPNekar@users.noreply.github.com>
2022-01-14 03:12:06 +0530

    Done

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,16 @@
 
 
 // export class using module.exports
+import Shape from './shape';
+var shape = new Shape();
+
+export class Circle extends shape{
+    constructor(color,radius){
+        super(color);
+        this.radius = radius;
+    }
+
+    calculateArea(){
+        console.log(3.14 * this.radius);
+    }
+}
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
 
 // export class using module.exports
+export class Shape{
+    constructor(color){
+        this.color = color
+    }
+    drawShape(){
+        console.log("");
+    }
+
+    calculateArea(){
+        console.log("");
+    }
+}
